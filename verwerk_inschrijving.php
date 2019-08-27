0f218b94bf6e309dd51f89c95a174e04826f15ab
Kjell Roelofs <k.roelofs@DMN-DCAGROEP.local>
2019-08-27 09:53:05 +0200

    Initial commit

---
+++ verwerk_inschrijving.php
@@ -0,0 +1,32 @@
+<?php
+
+//POST-ARRAY UITLEZEN
+$voornaam = $_POST['voornaam'];
+$tussenvoegsel = $_POST['tussenvoegsel'];
+$achternaam = $_POST['achternaam'];
+$mailadres = $_POST['mailadres'];
+
+// DATA IN DE DATABASE STOPPEN
+
+//1. verbinding maken met de database
+     $dbc = mysqli_connect('localhost', 'root', '', 'webapp') or die('error connencting');
+//2. opdracht schrijven voor de database
+     $query = "INSERT INTO nieuwsbrief_opdracht2 VALUES (0 , '$voornaam', '$tussenvoegsel' , '$achternaam', '$mailadres')";
+//3. opdracht uitvoeren
+     $result = mysqli_query($dbc , $query) or die ('error query');
+//4. verbinding verbreken
+     mysqli_close($dbc);
+
+
+// BEVESTIGEN DAT DATA IN DATABASE STAAT
+if ($result) {
+    echo 'de volgende gegevens zijn toegevoegd aan de database: <br>';
+    echo $voornaam . '<br>';
+    echo $tussenvoegsel . '<br>';
+    echo $achternaam . '<br>';
+    echo $mailadres . '<br>';
+} else{
+    echo 'sorry er is iets misgegaan probeer het opnieuw';
+}
+
+?>
